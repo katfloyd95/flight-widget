6d0e456adfa060eaf165e0995f5fb12c970a7646
Katarina <katarina.floyd1@gmail.com>
2022-09-30 14:15:11 +0200

    add backend server

--- server.js
+++ server.js
@@ -3,3 +3,24 @@ const axios = require('axios').default;
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+
+const app = express();
+
+app.get('/flights', (req, res) => {
+  const options = {
+    url: `${process.env.URL}?page-size=6`,
+    headers: {
+      accept: 'application/json',
+      'X-Cassandra-Token': process.env.TOKEN
+    }
+  }
+
+  axios.request(options).then(response => {
+    console.log(response.data);
+    res.json(response.data); // also shows in the browser
+  }).catch(error => {
+    console.log(error);
+  })
+})
+
+app.listen(PORT, () => console.log('running on port' + PORT));
